8531ad0412f084e7bac7af7f8f2601595c19a89b
esyeda <esyeda6125@gmail.com>
2025-07-09 17:52:38 +0000

    Fix homepage return statement

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return 'Hello is this working?|Auto-deploy is working!'
+    return 'Hello is this working? Auto-deploy is working!'
 
 @app.route("/update_server", methods=['POST'])
 def webhook():
